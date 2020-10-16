bbfc5bf1abb0af5c334ac487f18c3cfe6d55cab5
Afiq Harith <afiqharith05@gmail.com>
2020-10-16 22:07:53 +0800

    linting

--- setup/config.py
+++ setup/config.py
@@ -1,40 +1,40 @@
 # App configuration
-CAMERA 		= False
+CAMERA = False
 
 # Model path
-MODELPATH  	= "utils/model/"
-WEIGHTS     = "yolov3.weights"
-CFG         = "yolov3.cfg"
-COCONAMES   = "coco.names"
+MODELPATH = "utils/model/"
+WEIGHTS = "yolov3.weights"
+CFG = "yolov3.cfg"
+COCONAMES = "coco.names"
 
 # Video path
-FOLDERNAME  = "videos/"
-VIDEONAME   = "TownCentre.mp4"
-WIDTH 		= 1280
-HEIGHT 		= 720
-DISTANCE 	= 68.5
+FOLDERNAME = "videos/"
+VIDEONAME = "TownCentre.mp4"
+WIDTH = 1280
+HEIGHT = 720
+DISTANCE = 68.5
 
 '''
 # PETS2009
-FOLDERNAME  = "videos/"
-VIDEONAME   = "PETS2009.mp4"
-WIDTH 		= 1280
-HEIGHT 		= 720
-DISTANCE 	= 70
+FOLDERNAME = "videos/"
+VIDEONAME = "PETS2009.mp4"
+WIDTH = 1280
+HEIGHT = 720
+DISTANCE = 70
 
 # VIRAT
-FOLDERNAME  = "videos/"
-VIDEONAME   = "VIRAT.mp4"
-WIDTH 		= 1280
-HEIGHT 		= 720
-DISTANCE 	= 55
+FOLDERNAME = "videos/"
+VIDEONAME = "VIRAT.mp4"
+WIDTH = 1280
+HEIGHT = 720
+DISTANCE = 55
 '''
 
 # Colors configuration
-GREEN 	    = (0,255,0)
-RED 	    = (0,0,255)
-YELLOW 	    = (0,255,255)
-WHITE 	    = (255,255,255)
-ORANGE 	    = (0,165,255)
-BLUE 	    = (255,0,0)
-GREY 	    = (192,192,192)
+GREEN = (0,255,0)
+RED = (0,0,255)
+YELLOW = (0,255,255)
+WHITE = (255,255,255)
+ORANGE = (0,165,255)
+BLUE = (255,0,0)
+GREY = (192,192,192)
