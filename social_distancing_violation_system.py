725b90a3fa2cc6ef12bea073c17b627feb9f42d7
Afiq Harith <afiqharith05@gmail.com>
2021-05-26 17:52:58 +0800

    logging

--- social_distancing_violation_system.py
+++ social_distancing_violation_system.py
@@ -257,11 +257,11 @@ class App:
                 ymin = detected_bbox[i][1]
                 xmax = detected_bbox[i][2]
                 ymax = detected_bbox[i][3]
-                # self.cross_line(xmin, ymin, xmax, ymax,GREY)
+                self.cross_line(xmin, ymin, xmax, ymax,GREY)
                 # Else, wrap red bbox
                 if detected_bbox_colors[i]:
-                    # self.cross_line(xmin, ymin, xmax, ymax, RED)
-                    self.rect_detection_box(self.frame, xmin, ymin, xmax, ymax, RED)
+                    self.cross_line(xmin, ymin, xmax, ymax, RED)
+                    # self.rect_detection_box(self.frame, xmin, ymin, xmax, ymax, RED)
                     label = "high".upper()
                     labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_DUPLEX, 0.5, 1)
 
@@ -272,8 +272,8 @@ class App:
 
                 # If euclidean distance less than (<) DISTANCE, wrap black bbox
                 else:
-                    # self.cross_line(xmin, ymin, xmax, ymax, BLACK)
-                    self.rect_detection_box(self.frame, xmin, ymin, xmax, ymax, BLACK)
+                    self.cross_line(xmin, ymin, xmax, ymax, BLACK)
+                    # self.rect_detection_box(self.frame, xmin, ymin, xmax, ymax, BLACK)
                     label = "low".upper()
                     labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_DUPLEX, 0.5, 1)
 
